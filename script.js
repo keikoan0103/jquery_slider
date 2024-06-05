4a316f7276b214f5b017ca5f0ac5a991dad784fb
keikoan0103 <167289236+keikoan0103@users.noreply.github.com>
2024-06-05 15:54:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript
+// console.log('Hello world!');
+
+$(function(){
+    $('.bxslider').bxSlider({
+        mode: 'horizontal',
+        captions: true,
+        auto: true,
+        speed: 500,
+        pause: 3000,
+        autoHover: true
+    });
+  });
